aa8a4db18991073ffee595d8bccfe9225fdba4d8
MD.MAHMUDUL HASAN <126802456+andmahmud@users.noreply.github.com>
2025-01-30 11:06:32 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:math_expressions/math_expressions.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(const GetMaterialApp(
     debugShowCheckedModeBanner: false,
     title: "Calculator",
     home: CalculatorApp(),
   ));
 }
 
-class CalculatorApp extends StatefulWidget {
-  const CalculatorApp({super.key});
+class CalculatorController extends GetxController {
+  var input = ''.obs;
+  var result = ''.obs;
 
-  @override
-  _CalculatorAppState createState() => _CalculatorAppState();
-}
-
-class _CalculatorAppState extends State<CalculatorApp> {
-  String _input = '';
-  String _result = '';
-
-  void _onButtonPressed(String value) {
-    setState(() {
-      if (value == "C") {
-        _input = '';
-        _result = '';
-      } else if (value == "=") {
-        try {
-          _result = _evaluateExpression(_input);
-        } catch (e) {
-          _result = "Error";
-        }
-      } else {
-        _input += value;
+  void onButtonPressed(String value) {
+    if (value == "C") {
+      input.value = '';
+      result.value = '';
+    } else if (value == "=") {
+      try {
+        result.value = evaluateExpression(input.value);
+      } catch (e) {
+        result.value = "Error";
       }
-    });
+    } else {
+      input.value += value;
+    }
   }
 
-  String _evaluateExpression(String expression) {
+  String evaluateExpression(String expression) {
     try {
       Parser parser = Parser();
       Expression exp = parser.parse(expression.replaceAll('x', '*'));
@@ -48,42 +40,15 @@ class _CalculatorAppState extends State<CalculatorApp> {
       return "Error";
     }
   }
+}
 
-  Widget _buildDisplay() {
-    return Expanded(
-      flex: 2,
-      child: Container(
-        padding: const EdgeInsets.all(20),
-        alignment: Alignment.bottomRight,
-        color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Text(
-              _input,
-              style: const TextStyle(fontSize: 40, color: Colors.black),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              _result,
-              style: const TextStyle(fontSize: 39, color: Colors.grey),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildButton(String text) {
-    return CalculatorButton(
-      text: text,
-      onTap: () => _onButtonPressed(text),
-    );
-  }
+class CalculatorApp extends StatelessWidget {
+  const CalculatorApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final CalculatorController controller = Get.put(CalculatorController());
+    
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -93,52 +58,43 @@ class _CalculatorAppState extends State<CalculatorApp> {
       ),
       body: Column(
         children: [
-          _buildDisplay(),
           Expanded(
-            flex: 1,
-            child: Row(
-              children: [
-                _buildButton("1"),
-                _buildButton("2"),
-                _buildButton("3"),
-                _buildButton("+"),
-              ],
+            flex: 2,
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              alignment: Alignment.bottomRight,
+              color: Colors.white,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Obx(() => Text(
+                        controller.input.value,
+                        style: const TextStyle(fontSize: 40, color: Colors.black),
+                      )),
+                  const SizedBox(height: 10),
+                  Obx(() => Text(
+                        controller.result.value,
+                        style: const TextStyle(fontSize: 39, color: Colors.grey),
+                      )),
+                ],
+              ),
             ),
           ),
-          Expanded(
-            flex: 1,
-            child: Row(
-              children: [
-                _buildButton("4"),
-                _buildButton("5"),
-                _buildButton("6"),
-                _buildButton("-"),
-              ],
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Row(
-              children: [
-                _buildButton("7"),
-                _buildButton("8"),
-                _buildButton("9"),
-                _buildButton("*"),
-              ],
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Row(
-              children: [
-                _buildButton("0"),
-                _buildButton("C"),
-                _buildButton("="),
-                _buildButton("/"),
-              ],
-            ),
-          ),
+          _buildButtonRow(controller, ["1", "2", "3", "+"]),
+          _buildButtonRow(controller, ["4", "5", "6", "-"])
+          _buildButtonRow(controller, ["7", "8", "9", "*"])
+          _buildButtonRow(controller, ["0", "C", "=", "/"])
         ],
+      ),
+    );
+  }
+
+  Widget _buildButtonRow(CalculatorController controller, List<String> texts) {
+    return Expanded(
+      flex: 1,
+      child: Row(
+        children: texts.map((text) => CalculatorButton(text: text, onTap: () => controller.onButtonPressed(text))).toList(),
       ),
     );
   }
@@ -159,16 +115,14 @@ class CalculatorButton extends StatelessWidget {
           padding: const EdgeInsets.all(8.0),
           child: Container(
             alignment: Alignment.center,
-            
             decoration: BoxDecoration(
               color: Colors.teal,
-              borderRadius: BorderRadius.circular(20), // Circular border radius
+              borderRadius: BorderRadius.circular(20),
               boxShadow: [
                 BoxShadow(
-                  // ignore: deprecated_member_use
-                  color: Colors.grey.withOpacity(0.3), // Shadow color
-                  blurRadius: 20, // Spread of the shadow
-                  offset: const Offset(2, 2), // Position of the shadow
+                  color: Colors.grey.withOpacity(0.3),
+                  blurRadius: 20,
+                  offset: const Offset(2, 2),
                 ),
               ],
             ),
@@ -182,4 +136,3 @@ class CalculatorButton extends StatelessWidget {
     );
   }
 }
-
